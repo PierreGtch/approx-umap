914f79aa758f30e71f6ee5a7430d98197438c627
pctwass <peterw10001@gmail.com>
2024-04-12 13:06:50 +0200

    expanded test clause

--- tests/test_approx_umap.py
+++ tests/test_approx_umap.py
@@ -15,4 +15,6 @@ def test_not_enough_neigh():
     X = np.random.rand(5, 10)
     aumap = ApproxUMAP(n_neighbors=10)
     emb = aumap.fit_transform(X)
+    emb2 = aumap.transform(X)
     assert emb.shape == (5, 2)
+    assert emb2.shape == (5, 2)
